a755a8dc467018b8272e9f420b809b0270a8897b
Tyler <tylermw@gmail.com>
2020-02-23 19:37:49 -0500

    adjustedcranlogs 0.2.0: Changed method used to calculate minimum number of shared daily downloads from 0.05 quantile to the daily minimum.
    -Improved documentation
    -Fixed error when no updates

--- R/adj_cran_downloads.R
+++ R/adj_cran_downloads.R
@@ -1,7 +1,7 @@
 #'@title Adjusted Cran Downloads
 #'
 #'@description Removes daily shared downloads amongst CRAN packages and re-download/CRAN mirror spikes associated with
-#'package updates.
+#'package updates. The function samples a number of packages from the CRAN and finds the minimum
 #'
 #'@param packages A character vector, the packages to query.
 #'@param when \code{last-day}, \code{last-week} or \code{last-month}.
@@ -10,10 +10,13 @@
 #'   \code{last-day}. It is ignored if \code{when} is given.
 #'@param to End date, in \code{yyyy-mm-dd} format, or
 #'   \code{last-day}. It is ignored if \code{when} is given.
-#'@param removeupdatespikes Default TRUE. Removes the spike in downloads due to automatic re-downloads
-#'and CRAN mirrors associated with a package update. Replaces value with the median download value across the measurement period.
-#'@param numbercomparison Default 100. The number of random packages to inspect to determine the minimum number of downloads in any single day.
-#'@param packagequantile Default 0.05. The quantile defining the "minimum" number of downloads on a single day.
+#'@param remove_update_spikes Default `TRUE`. Removes the spike in downloads due to automatic re-downloads
+#'and CRAN mirrors associated with a package update. This replaces the number of downloads on an update day
+#'or the day following with the median download value across the measurement period. You can set this to `FALSE` if you do
+#'not see spikes in downloads on days before or after you update to accurately capture downloads during those days.
+#'@param number_to_compare Default `100`. The number of random packages to inspect to determine the minimum number of downloads in any single day.
+#'@param value_quantile Default `NULL`. By default, the function uses the minimum number of downloads. Setting a number here
+#'(between 0 and 1) instead returns the nth lowest number of downloads, where n = `value_quantile` * `number_to_compare`.
 #'@return A data frame of downloads and total downloads by package.
 #'   \item{\code{date}}{Day of the downloads, it is a Date object.}
 #'   \item{\code{package}}{The package. This column is missing if
@@ -32,7 +35,13 @@
 #'adj_cran_downloads("skpr",when="last-month")
 #'adj_cran_downloads(c("skpr","AlgDesign"),when="last-month")}
 
-adj_cran_downloads = function(packages, when, from, to, removeupdatespikes = TRUE, numbercomparison = 100, packagequantile=0.05) {
+adj_cran_downloads = function(packages, when, from, to, remove_update_spikes = TRUE,
+                              number_to_compare = 100, value_quantile = NULL) {
+
+  lowest_type = "min"
+  if(!is.null(value_quantile)) {
+    lowest_type = "quantile"
+  }
 
   packagedf = packagepublished
 
@@ -44,8 +53,7 @@ adj_cran_downloads = function(packages, when, from, to, removeupdatespikes = TRU
   packageurls = paste0("https://cran.r-project.org/web/packages/",packages,"/index.html")
   packagearchiveurls = paste0("https://cran.r-project.org/src/contrib/Archive/",packages,"/")
 
-  listupdates = list()
-
+  listupdates = vector(mode ="list", length = length(packages))
   for(package in 1:length(packages)) {
     htmltemp = tryCatch(xml2::read_html(packagearchiveurls[package]),error = function(e) {})
 
@@ -86,7 +94,10 @@ adj_cran_downloads = function(packages, when, from, to, removeupdatespikes = TRU
   packageupdatelist = list()
 
   for(package in 1:length(packagedates)) {
-    packageupdatelist[[package]] = data.frame(package=packages[package],date=as.Date(unlist(lapply(packagedates[[package]],lubridate::date)),origin = "1970-01-01"),stringsAsFactors = FALSE)
+    packageupdatelist[[package]] = data.frame(package=packages[package],
+                                              date=as.Date(unlist(lapply(packagedates[[package]],lubridate::date)),
+                                                           origin = "1970-01-01"),
+                                              stringsAsFactors = FALSE)
   }
 
   packageupdatedf = do.call(rbind,packageupdatelist)
@@ -97,10 +108,10 @@ adj_cran_downloads = function(packages, when, from, to, removeupdatespikes = TRU
 
   priorpackages = dplyr::filter(packagedf, published < earliestpub)
 
-  if(nrow(priorpackages) > numbercomparison) {
-    sampledpriorpackages =  sample_n(priorpackages,numbercomparison)
+  if(nrow(priorpackages) > number_to_compare) {
+    sampledpriorpackages =  sample_n(priorpackages,number_to_compare)
   } else {
-    sampledpriorpackages =  sample_n(row(priorpackages),numbercomparison,replace = TRUE)
+    sampledpriorpackages =  sample_n(row(priorpackages),number_to_compare,replace = TRUE)
   }
 
   packagenames = sampledpriorpackages$package
@@ -109,24 +120,35 @@ adj_cran_downloads = function(packages, when, from, to, removeupdatespikes = TRU
 
   comparepackagelogs =  cranlogs::cran_downloads(packagenames,when=when,from=from,to=to)
 
-  if(removeupdatespikes) {
+  compare_function = function(count, quantile, compare_type) {
+    if(compare_type != "min") {
+      return(stats::quantile(count,quantile,type=1))
+    } else {
+      return(min(count,na.rm = TRUE))
+    }
+  }
+
+  if(remove_update_spikes) {
     comparepackagelogs %>%
       group_by(date) %>%
-      summarise(mindownloads = stats::quantile(count,packagequantile,type=1)) %>%
+      summarise(mindownloads = compare_function(count, value_quantile, lowest_type)) %>%
       left_join(packagelogs,by=c("date")) %>%
       full_join(packageupdatedf,by=c("date","package")) %>%
       mutate(updateday = ifelse(is.na(updateday),FALSE,TRUE)) %>%
       mutate(adjusted_downloads = ifelse(count - mindownloads < 0,0,count - mindownloads)) %>%
       arrange(package) %>%
-      mutate(adjusted_downloads = ifelse(updateday | lag(updateday) & package == lag(package), stats::median(adjusted_downloads), adjusted_downloads)) %>%
+      mutate(adjusted_downloads = ifelse(updateday | lag(updateday) & package == lag(package),
+                                         stats::median(adjusted_downloads),
+                                         adjusted_downloads)) %>%
       filter(!is.na(adjusted_downloads)) %>%
       group_by(package) %>%
       mutate(adjusted_total_downloads = cumsum(adjusted_downloads), total_downloads = cumsum(count)) %>%
-      ungroup() -> finaldf
+      ungroup() ->
+    finaldf
   } else {
     comparepackagelogs %>%
       group_by(date) %>%
-      summarise(mindownloads = stats::quantile(count,packagequantile,type=1)) %>%
+      summarise(mindownloads = compare_function(count, value_quantile, lowest_type)) %>%
       left_join(packagelogs,by=c("date")) %>%
       full_join(packageupdatedf,by=c("date","package")) %>%
       mutate(updateday = ifelse(is.na(updateday),FALSE,TRUE)) %>%
@@ -135,7 +157,8 @@ adj_cran_downloads = function(packages, when, from, to, removeupdatespikes = TRU
       filter(!is.na(adjusted_downloads)) %>%
       group_by(package) %>%
       mutate(adjusted_total_downloads = cumsum(adjusted_downloads), total_downloads = cumsum(count)) %>%
-      ungroup() -> finaldf
+      ungroup() ->
+    finaldf
   }
   return(finaldf)
 }
